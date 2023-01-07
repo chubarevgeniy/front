927952b7f3599a300b3717cbacb45b40c3f2ec53
chubarevgeniy <jeakven2014@gmail.com>
2023-01-07 17:15:29 +0100

    delete old material ui

--- react-chat/src/components/InputMessage/InputMessage.jsx
+++ react-chat/src/components/InputMessage/InputMessage.jsx
@@ -12,7 +12,7 @@ export default function InputMessage(props) {
     const [inputValue, setInputValue] = useState('');
     return (
         <div>
-            <form id={props.id} onSubmit={(e)=>{sendMessage(e,inputValue,props.Mes); setInputValue('')}}>
+            <form id={props.id} className = "textareaholder" onSubmit={(e)=>{sendMessage(e,inputValue,props.Mes); setInputValue('')}}>
                 <textarea className="form-input" type="text" value = {inputValue} onChange={(e)=>setInputValue(e.target.value)}>
                 </textarea>
             </form>
